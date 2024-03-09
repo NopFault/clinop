c6da5bc3757fe8a06e6610f7c1c5588f2f5fe84e
x90 <x90@x90s-MacBook-Air.local>
2024-03-09 20:45:28 +0200

    Init

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use std::str::FromStr;
+
+struct CliNop {
+    args: Vec<String>,
+}
+
+impl CliNop {
+    fn new(args: Vec<String>) -> CliNop {
+        CliNop { args }
+    }
+
+    fn get<T: FromStr>(&self, arg: &str) -> Option<T> {
+        for (indx, argument) in self.args.iter().enumerate() {
+            if argument.trim_start_matches('-') == arg
+                && indx + 1 < self.args.len()
+                && !self.args[indx + 1].starts_with("--")
+            {
+                return T::from_str(self.args[indx + 1].as_str()).ok();
+            }
+        }
+        None
+    }
+}
